d696dbddbb50f4cc672339611be3e63fc04b0567
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-18 21:22:30 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is a lowercase
+ * @c: first parameter
+ * Return: 1 if c is lowercase.
+ */
+
+int _islower(int c);
+{
+	return (c >= 'a' && c <= 'z');
+}
