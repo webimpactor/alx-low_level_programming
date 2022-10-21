0637ff7cb231fa734e501115cfcf3d68e4f72188
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-20 21:56:39 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mulc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul -function for multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int p;
+
+	p = a * b;
+
+	return (p);
+}
