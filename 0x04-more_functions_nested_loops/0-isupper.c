ae427506fee6896802ebff2555d2eea14a4f8425
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-20 21:30:45 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int c;
+	int v;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		v = 1;
+	}
+	else
+	{
+		v = 0;
+	}
+	return (v);
+}
