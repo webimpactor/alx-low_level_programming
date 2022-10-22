0214af0244e84a8c0d7339e9667e5dd2d3e12eb2
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-21 18:42:41 -0700

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,12 +11,19 @@ void print_square(int size)
 	int x;
 	int y;
 
-	for (y = 0; y < size; y++)
+	if (size > 0)
 	{
-		for (x = 0; x < size; x++)
+		for (y = 0; y < size; y++)
 		{
-			_putchar('#');
+			for (x = 0; x < size; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
