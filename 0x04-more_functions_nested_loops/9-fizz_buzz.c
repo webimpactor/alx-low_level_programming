81e8ef1cbad63abea049e2fb10f244f1db224ed7
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-21 19:42:22 -0700

    Print numbers with Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			printf("Fizz ");
+		}
+		else if (((i % 3) != 0) && ((i % 5) == 0))
+		{
+			printf("Buzz ");
+		}
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
