b1cd220d37d203c91546107b0109dcfd67fa9a2b
WebImpactor <gloryjaiyeoba@gmail.com>
2022-10-24 20:45:18 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string input
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
