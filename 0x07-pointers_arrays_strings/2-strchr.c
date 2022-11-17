31f1f259282c489907e29be284493b6f89aa2210
WebImpactor <gloryjaiyeoba@gmail.com>
2022-11-17 08:14:46 -0800

    Locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: pointer to source string
+ * @c: character to be located
+ * Return: Pointer to first occurence of char c
+ * or Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
